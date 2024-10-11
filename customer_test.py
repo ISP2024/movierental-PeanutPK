55f82c9f535cc4fb181dd77d163091cd15286c49
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-12 01:40:35 +0700

    Implement testcase for computing total charges

--- customer_test.py
+++ customer_test.py
@@ -1,38 +1,46 @@
 import re
-import unittest 
+import unittest
 from customer import Customer
 from rental import Rental
 from movie import Movie
 
-class CustomerTest(unittest.TestCase): 
+
+class CustomerTest(unittest.TestCase):
     """ Tests of the Customer class"""
-    
+
     def setUp(self):
-    	"""Test fixture contains:
-    	
-    	c = a customer
-    	movies = list of some movies
-    	"""
-    	self.c = Customer("Movie Mogul")
-    	self.new_movie = Movie("Mulan", Movie.NEW_RELEASE)
-    	self.regular_movie = Movie("CitizenFour", Movie.REGULAR)
-    	self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
-    	
-    @unittest.skip("No convenient way to test")
-    def test_billing():
-    	# no convenient way to test billing since its buried in the statement() method.
-    	pass
-    
+        """Test fixture contains:
+
+        c = a customer
+        movies = list of some movies
+        """
+        self.c = Customer("Movie Mogul")
+        self.new_movie = Movie("Mulan", Movie.NEW_RELEASE)
+        self.regular_movie = Movie("CitizenFour", Movie.REGULAR)
+        self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
+
+    def test_billing(self):
+        """test for computing total charges"""
+        self.c.add_rental(Rental(self.new_movie, 5))
+        self.c.add_rental(Rental(self.childrens_movie, 4))
+        # new movie 15.0 + children 3.0
+        self.assertEqual(self.c.get_billing(), 18.0)
+
+        self.c.add_rental(Rental(self.new_movie, 5))
+        self.c.add_rental(Rental(self.childrens_movie, 4))
+        # new movie 15.0 + children 3.0 + old charges
+        self.assertEqual(self.c.get_billing(), 36.0)
+
     def test_statement(self):
-    	stmt = self.c.statement()
-    	# get total charges from statement using a regex
-    	pattern = r".*Total [Cc]harges\s+(\d+\.\d\d).*"
-    	matches = re.match(pattern, stmt, flags=re.DOTALL)
-    	self.assertIsNotNone(matches)
-    	self.assertEqual("0.00", matches[1])
-    	# add a rental
-    	self.c.add_rental(Rental(self.new_movie, 4)) # days
-    	stmt = self.c.statement()
-    	matches = re.match(pattern, stmt.replace('\n',''), flags=re.DOTALL)
-    	self.assertIsNotNone(matches)
-    	self.assertEqual("12.00", matches[1])
+        stmt = self.c.statement()
+        # get total charges from statement using a regex
+        pattern = r".*Total [Cc]harges\s+(\d+\.\d\d).*"
+        matches = re.match(pattern, stmt, flags=re.DOTALL)
+        self.assertIsNotNone(matches)
+        self.assertEqual("0.00", matches[1])
+        # add a rental
+        self.c.add_rental(Rental(self.new_movie, 4))  # days
+        stmt = self.c.statement()
+        matches = re.match(pattern, stmt.replace('\n', ''), flags=re.DOTALL)
+        self.assertIsNotNone(matches)
+        self.assertEqual("12.00", matches[1])
