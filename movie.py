da88df9ee0ece779f422358d2f4aceaa2888e4dc
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-12 23:26:05 +0700

    Make movie class compute their own price and point

--- movie.py
+++ movie.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Movie:
     """
     A movie available for rent.
@@ -6,18 +9,48 @@ class Movie:
     REGULAR = 0
     NEW_RELEASE = 1
     CHILDRENS = 2
-    
+
     def __init__(self, title, price_code):
         # Initialize a new movie. 
         self.title = title
         self.price_code = price_code
 
+    def get_rental_points(self, days):
+        if self.get_price_code() == self.NEW_RELEASE:
+            # New release earns 1 point per day rented
+            return days
+        else:
+            # Other rentals get only 1 point
+            return 1
+
+    def get_price(self, days):
+        """Return a calculated price for a rental."""
+        amount = 0
+        if self.get_price_code() == self.REGULAR:
+            # Two days for $2, additional days 1.50 per day.
+            amount = 2.0
+            if days > 2:
+                amount += 1.5 * (days - 2)
+        elif self.get_price_code() == self.CHILDRENS:
+            # Three days for $1.50, additional days 1.50 per day.
+            amount = 1.5
+            if days > 3:
+                amount += 1.5 * (days - 3)
+        elif self.get_price_code() == self.NEW_RELEASE:
+            # Straight $3 per day charge
+            amount = 3 * days
+        else:
+            log = logging.getLogger()
+            log.error(
+                f"Movie {self} has unrecognized priceCode {self.get_price_code()}")
+        return amount
+
     def get_price_code(self):
         # get the price code
         return self.price_code
-    
+
     def get_title(self):
         return self.title
-    
+
     def __str__(self):
         return self.title
