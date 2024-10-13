306bb4fb9eaf94c9cbfd664bf6d37773c6c2d9ac
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-13 11:49:39 +0700

    Add some comments to the testcase

--- rental_test.py
+++ rental_test.py
@@ -20,14 +20,17 @@ class RentalTest(unittest.TestCase):
 
     def test_rental_price(self):
         """test rental price for each movie attribute"""
+        # New movies
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_price(), 3.0)
         rental = Rental(self.new_movie, 5)
         self.assertEqual(rental.get_price(), 15.0)
+        # Regular movies
         rental = Rental(self.regular_movie, 1)
         self.assertEqual(rental.get_price(), 2.0)
         rental = Rental(self.regular_movie, 5)
         self.assertEqual(rental.get_price(), 6.5)
+        # Children movies
         rental = Rental(self.childrens_movie, 1)
         self.assertEqual(rental.get_price(), 1.5)
         rental = Rental(self.childrens_movie, 5)
