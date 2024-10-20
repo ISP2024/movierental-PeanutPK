3accf294a8cd6cf27dd582e459a9a25d8da60112
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-20 13:58:41 +0700

    Modified main to use MovieCatalog

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 # Demonstrate the movie rental code.
 # Create a customer with some movies and print a statement.
+import logging
 
-from movie import Movie
+from movie import Movie, MovieCatalog
 from rental import Rental
 from customer import Customer
 
 
 def make_movies():
     """Some sample movies."""
+    catalog = MovieCatalog()
     movies = [
-        Movie("Air", Movie.NEW_RELEASE),
-        Movie("Oppenheimer", Movie.REGULAR),
-        Movie("Frozen", Movie.CHILDREN),
-        Movie("Bitconned", Movie.NEW_RELEASE),
-        Movie("Particle Fever", Movie.REGULAR)
+        catalog.get_movie("Air"),
+        catalog.get_movie("Oppenheimer"),
+        catalog.get_movie("Frozen"),
+        catalog.get_movie("Bitconned"),
+        catalog.get_movie("Particle Fever")
     ]
     return movies
 
@@ -22,7 +24,14 @@ if __name__ == '__main__':
     # Create a customer with some rentals
     customer = Customer("Edward Snowden")
     days = 1
+    index = 0
+    price_code = [Rental.NEW_RELEASE, Rental.REGULAR, Rental.CHILDREN]
     for movie in make_movies():
-        customer.add_rental(Rental(movie, days))
+        if movie:
+            customer.add_rental(Rental(movie, days, price_code[index % 3]))
+        else:
+            log = logging.getLogger("MovieFinder")
+            log.info("Movie not found")
         days = (days + 2) % 5 + 1
+        index += 1
     print(customer.statement())
