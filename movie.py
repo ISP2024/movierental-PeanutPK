485172649ac579c20f6f004d52d6ef9d3480047d
Phiranath PO-NGERN <phukaow18@gmail.com>
2024-10-20 10:50:48 +0700

    Fix Lazy Class by adding attributes and change Movie class to frozen dataclass

--- movie.py
+++ movie.py
@@ -1,14 +1,21 @@
+from collections import Collection
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
+    title: str
+    year: int
+    genre: Collection[str]
 
-    def __init__(self, title):
-        # Initialize a new movie. 
-        self.title = title
+    def is_genre(self, genre: str) -> bool:
+        return genre.lower() in self.genre
 
     def get_title(self):
         return self.title
 
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
